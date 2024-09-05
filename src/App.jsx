1213717e31c3eddab54cbc93517fde2c91b6545d
Alexis Martinez <jhonyalexis100@gmail.com>
2024-09-05 16:15:22 -0500

    Logo and main styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
-
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
+    <div className='task-app'>
+      <div className='freecodecamp-logo-container'>
+        <img
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt="Logo" 
+        />
+
+      </div>
       
-    </>
+    </div>
   )
 }
 
